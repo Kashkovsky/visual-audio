051cbdb1978fdec0f8013b264b9cb25793e4d110
Denys Kashkovskyi <denys.kashkovskyi@evry.com>
2021-11-21 11:37:09 +0200

    feat(amorph): color options

--- packages/visuals/src/elements/amorph/amorph.ts
+++ packages/visuals/src/elements/amorph/amorph.ts
@@ -13,13 +13,19 @@ export namespace Amorph {
     readonly displasementStrength?: number
     /** @default 0.5 */
     readonly displasementFrequency?: number
+    /** @default 0 */
+    readonly distance?: number
+    /** @default false */
+    readonly colored?: boolean
   }
   export const create: AnimatedElement.Factory<Config> =
     ({
       distortionFrequency = 5,
       rotationFactor = 1,
       displasementStrength = 0.02,
-      displasementFrequency = 0.5
+      displasementFrequency = 0.5,
+      distance = 0,
+      colored = false
     }) =>
     ({ camera, scene }) => {
       const camFactor = camera.position.z * 0.25
@@ -31,11 +37,12 @@ export namespace Amorph {
           vTime: { value: 0 },
           vDistortionFrequency: { value: distortionFrequency },
           vDisplacementFrequency: { value: 3 },
-          vDisplacementStrength: { value: 0.2 }
+          vDisplacementStrength: { value: 0.2 },
+          colored: { value: colored }
         }
       })
       const mesh = new THREE.Mesh(geometry, material)
-      mesh.position.z = -0.5
+      mesh.position.z = distance
       scene.add(mesh)
 
       const update = (velocity: number) => {
--- packages/visuals/src/elements/amorph/shaders/fragment.glsl
+++ packages/visuals/src/elements/amorph/shaders/fragment.glsl
@@ -1,6 +1,13 @@
+uniform bool colored;
+
 varying vec3 vNormal;
 varying float vPerlinStrength;
 
 void main(){
-    gl_FragColor = vec4(vNormal * vPerlinStrength * 2., 1.);
+    if (colored)
+		gl_FragColor = vec4(vNormal * vPerlinStrength * 2., 1.);
+	else {
+		float color = vNormal.z * vPerlinStrength * 2.;
+		gl_FragColor = vec4(color, color, color, 1.);
+	}
 }
